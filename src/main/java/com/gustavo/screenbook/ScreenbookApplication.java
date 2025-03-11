1b5ba9752bccb790979cb5577e1c2a602222b37b
o-gustavolopes <gustavolopesmartins2506@gmail.com>
2025-03-11 16:01:21 -0300

    Primeiro commit: config inicial springboot

---
+++ src/main/java/com/gustavo/screenbook/ScreenbookApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.screenbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/screenbook/ScreenbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.screenbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
